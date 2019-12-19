d070e6c520c2615b0abcb0617fa1301b5a28fc75
stevenhsu <mingje1104@gmail.com>
2019-12-19 17:43:02 +0800

    add size_to_int()

--- check_time.sikuli/check_time.py
+++ check_time.sikuli/check_time.py
@@ -1,7 +1,6 @@
 from sikuli import *
 from library_qsync import *
-
-
+"""
 def counter_surplus_no(data_type, counter_type, path):
     try:
         surplus_no = counter_data(data_type, counter_type, path)
@@ -13,3 +12,9 @@ path1 = "C:\\Users\\" + get_pc_info("user_name") + "\\@Qsync_test\\.qsync"
 path = "D:\\test"
 
 print(counter_surplus_no("total", "all", path1))
+"""
+
+def size_to_int(size):
+    size = size.replace(',', '')
+    return int(size)
+
